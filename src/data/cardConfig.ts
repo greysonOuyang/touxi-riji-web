4d4acd228ff034752220ae0c9bc04a4d1d74bb1e
greyson <ouyangguanling@gmail.com>
2024-10-21 14:11:33 +0800

    喝水以及其他调整

--- src/data/cardConfig.ts
+++ src/data/cardConfig.ts
@@ -1,14 +1,15 @@
 export const cardConfig = [
     {
-      id: 1,
-      type: 'weight',
-      isFullWidth: true,
-    },
-    {
         id: 1,
         type: 'water',
         isFullWidth: false,
       },
+    {
+      id: 2,
+      type: 'weight',
+      isFullWidth: true,
+    },
+    
     // 可以继续添加更多卡片配置
     // {
     //   id: 3,
