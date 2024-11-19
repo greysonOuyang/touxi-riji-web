533f37dc69e950a464f06966c894654292bc7567
greyson <greyson_silence@proton.me>
2024-11-19 11:10:24 +0800

    按钮样式

--- src/components/AddButton/index.tsx
+++ src/components/AddButton/index.tsx
@@ -1,12 +1,27 @@
-import { View } from '@tarojs/components';
-import './index.scss';
+import React, { useState } from "react";
+import { View } from "@tarojs/components";
+import "./index.scss";
 
-const AddButton = ({ size = 32, onClick, className = '', style = {} }) => {
+const AddButton = ({ size = 32, onClick, className = "", style = {} }) => {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleClick = (event) => {
+    event.stopPropagation(); // 防止事件冒泡
+    setIsActive(true); // 设置按钮为活动状态
+
+    if (onClick) onClick(); // 执行传入的点击函数
+
+    // 恢复状态
+    setTimeout(() => {
+      setIsActive(false);
+    }, 150); // 动效持续时间
+  };
+
   return (
     <View
-      className={`add-button ${className}`}
+      className={`add-button ${className} ${isActive ? "active" : ""}`}
       style={{ width: `${size}px`, height: `${size}px`, ...style }}
-      onClick={onClick}
+      onClick={handleClick}
     >
       <View className="plus-icon" />
     </View>
